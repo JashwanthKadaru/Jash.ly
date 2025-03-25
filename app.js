a457b52b6c3d6b1af4a3abd1eb891c945e0afb8f
Jashwanth Kadaru <jashwanthkadaru2021@gmail.com>
2025-03-25 09:22:38 +0530

    Fixing rate-limiter

--- app.js
+++ app.js
@@ -10,8 +10,6 @@ const path = require("path");
 const helmet = require('helmet');
 app.use(helmet());
 
-app.use(limiter);
-
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 
