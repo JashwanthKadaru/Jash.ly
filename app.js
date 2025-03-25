45bac2681f85e4c64190064d99554da1aae18e0e
Jashwanth Kadaru <jashwanthkadaru2021@gmail.com>
2025-03-25 09:20:55 +0530

    Fixing rate-limiter

--- app.js
+++ app.js
@@ -10,13 +10,6 @@ const path = require("path");
 const helmet = require('helmet');
 app.use(helmet());
 
-const rateLimit = require('express-rate-limit');
-
-const limiter = rateLimit({
-    windowMs: 2 * 60 * 60 * 1000, // 2 hours
-    max: 200, // Limit each IP to 200 requests per window
-});
-
 app.use(limiter);
 
 app.use(express.json()); 
